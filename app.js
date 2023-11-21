147c392972f1c634d7dce7e10905877c5ce8900a
Jake Parker <93437907+jakerbp@users.noreply.github.com>
2023-11-21 19:44:54 +0000

    Revert "5 - tests and feature code for get all articles"

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const { getTopics, getArticles, getArticle, getEndpoints } = require("./controllers/app.controller");
-const { handleCustomErrors, handleServerErrors, handle404, handlePsqlErrors } = require("./errors");
+const { getTopics, getArticle, getEndpoints } = require("./controllers/app.controller");
+const { handleCustomErrors, handleServerErrors, handlePsqlErrors } = require("./errors");
 const app = express();
+app.use(express.json())
 
 app.get('/api/topics', getTopics)
-
-app.get('/api/articles', getArticles)
 
 app.get('/api/articles/:article_id', getArticle)
 
 app.get('/api', getEndpoints)
 
-app.all('*', handle404)
 app.use(handleCustomErrors)
 app.use(handlePsqlErrors)
 app.use(handleServerErrors)
--- errors.js
+++ errors.js
@@ -6,10 +6,6 @@ exports.handleCustomErrors = (err, req, res, next) => {
   }
 };
 
-exports.handle404 = (req, res, next) => {
-  res.status(404).send({ msg: "Not found!" });
-}
-
 exports.handlePsqlErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Bad request!" });
